79d25fc5f2d201e4f41e2e1071d8b3253e932070
jonathan Fortunati <jonathan@testkube.io>
2026-02-02 14:22:40 -0500

    Changes to playwright command to generate junit results

--- test/playwright/playwright-load-balancing-demo/playwright.config.js
+++ test/playwright/playwright-load-balancing-demo/playwright.config.js
@@ -1,12 +1,17 @@
 const { defineConfig } = require('@playwright/test');
 
+// JUnit reporter enabled when output path is set (e.g. by Testkube workflow); includes execution time per test
+const reporters = process.env.PLAYWRIGHT_JUNIT_OUTPUT_NAME
+  ? ['list', ['junit', { outputFile: process.env.PLAYWRIGHT_JUNIT_OUTPUT_NAME }]]
+  : 'list';
+
 module.exports = defineConfig({
   testDir: './tests',
   timeout: 60 * 1000,
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: 0,
-  reporter: 'list',
+  reporter: reporters,
   use: {
     baseURL: 'https://testkube-test-page-lipsum.pages.dev/',
     video: 'off',
